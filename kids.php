d24e7e56e330322a77145db2bfe7e9a2a742704b
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-03 23:42:25 -0500

    Update kids.php

--- kids.php
+++ kids.php
@@ -1,1 +1,10 @@
+<?php
+require_once("util-db.php");
+require_once("model-kids.php");
 
+$pageTitle = "Kids";
+include "view-header.php";
+$kids = selectKids();
+include "view-kids.php";
+include "view-footer.php";
+?>
