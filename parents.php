c4c2473ecbce7ea1525a779aab0db0cfe866ad0c
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-03 23:31:22 -0500

    Update parents.php

--- parents.php
+++ parents.php
@@ -4,7 +4,7 @@ require_once("model-parents.php");
 
 $pageTitle = "Parents";
 include "view-header.php";
-$Parents = selectParents();
+$parents = selectParents();
 include "view-parents.php";
 include "view-footer.php";
 ?>
