a79701fb3f31d3a1ca4ef5b4f75fb4400c583f44
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 16:24:43 -0500

    Update view-parents-with-kids.php

--- view-parents-with-kids.php
+++ view-parents-with-kids.php
@@ -1,1 +1,31 @@
-
+<h1>Parents with Kids</h1>
+<div class="card-group">
+<?php
+while ($parent = $parents-> fetch_assoc())
+{
+?>
+<div class="card">
+<div class="card-body">
+<h5 class="card-title"><?php echo $parent['parent_name'];?></h5>
+<p class="card-text">
+<ul class="list-group">
+<?php
+$kids=selectKidsByParent($parent['parent_id']);
+while ($kid=$kids->fetch_assoc())
+{
+?>
+<li class="list-group-item"><?php echo $kid['kid_name'];?> -
+  <?php echo $kid['kid_birthyear'];?> </li>
+<?php
+}
+?>
+</ul>
+</p>
+<p class="card-text"><small class="text-body-secondary">City: <?php echo
+$parent['city_name'];?></small></p>
+</div>
+</div>
+<?php
+}
+?>
+</div>
