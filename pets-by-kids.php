73785b355134c68a945f41e511e83950ad72a510
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 16:38:29 -0500

    Update pets-by-kids.php

--- pets-by-kids.php
+++ pets-by-kids.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-pets-by-kids.php");
+$pageTitle = "Pets by Kids";
+include "view-header.php";
+$parents = selectPetsByKids();
+include "view-pets-by-kids.php";
+include "view-footer.php";
+?>
