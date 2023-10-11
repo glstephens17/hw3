173266c8dbf27ec178b5318d7787b5f812d5782b
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 17:21:25 -0500

    Update model-pets-by-kids.php

--- model-pets-by-kids.php
+++ model-pets-by-kids.php
@@ -2,7 +2,7 @@
 function selectPetsByKids($kid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT k.kid_id, kid_name, kid_birthyear, pet_name, pet_breed FROM `kids` k join `pet_ownership` ps on k.kid_id=ps.kid_id join `pet` p on p.pet_id=ps.pet_id where k.kid=?");
+        $stmt = $conn->prepare("SELECT k.kid_id, kid_name, kid_birthyear, pet_name, pet_breed FROM `kids` k join `pet_ownership` ps on k.kid_id=ps.kid_id join `pet` p on p.pet_id=ps.pet_id where k.kid_id=?");
         $stmt->bind_param("i", $kid);
         $stmt->execute();
         $result = $stmt->get_result();
