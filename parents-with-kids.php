0d98f9377467fff9cfd180f53871c8e5c3f0b104
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 16:27:30 -0500

    Update parents-with-kids.php

--- parents-with-kids.php
+++ parents-with-kids.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-parents-with-kids.php");
+$pageTitle = "Parents";
+include "view-header.php";
+$parents = selectParents();
+include "view-parents-with-kids.php";
+include "view-footer.php";
+?>
