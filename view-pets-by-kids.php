3310f0422946c0638bb1c55e49198e8fc36a4967
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 16:53:06 -0500

    Update view-pets-by-kids.php

--- view-pets-by-kids.php
+++ view-pets-by-kids.php
@@ -1,1 +1,31 @@
-
+<h1>Pets by Kids</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+         <th>Kid Id</th>
+      <th>Pet Name</th>
+      <th>Pet Breed</th>
+      <th>Kid Name</th>
+      <th>Kid Birth Year</th>
+      </tr>
+     
+    </thead>
+      <tbody>
+        <?php
+            while ($pet = $pets-> fetch_assoc())
+              {
+                ?>
+                  <tr>
+                    <td><?php echo $pet['pet_id'];?></td>
+                    <td><?php echo $pet['pet_name'];?></td>
+                    <td> <?php echo $pet['pet_breed'];?></td>
+                    <td><?php echo $pet['kid_name'];?></td>
+                    <td><?php echo $pet['kid_birthyear'];?></td>
+                  </tr>                  
+                <?php
+              }
+        ?>
+      </tbody>    
+  </table>
+</div>
