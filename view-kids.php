1e232ab6ec5f136b72f10b01ebdfba029823e553
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-11 17:08:36 -0500

    Update view-kids.php

--- view-kids.php
+++ view-kids.php
@@ -17,10 +17,12 @@
      <td><?php echo $kid['kid_id']; ?></td>
      <td><?php echo $kid['kid_name']; ?></td>
      <td><?php echo $kid['kid_birthyear']; ?></td>
-     <td><form method="post" action="pets-by-kids.php">
-                        <input type="hidden" name="kid" value="<?php echo $kids['kid_id'];?>"/>
-                            <button type="submit" class="btn btn-primary">Pets</button>
-                          </form></td>     
+     <td>
+  <form method="post" action="pets-by-kids.php">
+    <input type="hidden" name="kid" value="<?php echo kid['kid_id'];?>"/>
+    <button type="submit" class="btn btn-primary">Pets</button>
+  </form>
+     </td>     
    </tr>
  <?php  
  }
