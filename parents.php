bc1eafb1ae048a690d63a904326c509eac5db71a
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-18 21:49:51 -0500

    Update parents.php

--- parents.php
+++ parents.php
@@ -4,6 +4,14 @@ require_once("model-parents.php");
 
 $pageTitle = "Parents";
 include "view-header.php";
+
+if(isset($POST['actionType'])){
+  switch(($POST['actionType']){
+          case "Add":
+        insertParent($POST['pName'],$POST['pCity']);
+        break;
+  }
+}
 $parents = selectParents();
 include "view-parents.php";
 include "view-footer.php";
