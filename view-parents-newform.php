0fec30d5d717b05c30151da5ed7b6dd4394ccaf7
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-18 21:36:57 -0500

    Update view-parents-newform.php

--- view-parents-newform.php
+++ view-parents-newform.php
@@ -16,18 +16,14 @@
       <div class="modal-body">
         <form>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Parent Name</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <label for="pName" class="form-label">Parent Name</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
       </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Parent City</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="pCity" class="form-label">Parent City</label>
+    <input type="text" class="form-control" id="exampleInputPassword1">
   </div>
-  <div class="mb-3 form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
           </div>
         </div>
