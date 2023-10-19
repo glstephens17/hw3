47cafd0e3cabe58d369bb1ab247cc0465bb1d226
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-18 21:55:56 -0500

    Update view-parents-newform.php

--- view-parents-newform.php
+++ view-parents-newform.php
@@ -16,12 +16,12 @@
       <div class="modal-body">
         <form method="post" action="">
   <div class="mb-3">
-    <label for="pName" class="form-label">Parent Name</label>
-    <input type="text" class="form-control" id="pName" name="pName">
+    <label for="parentName" class="form-label">Parent Name</label>
+    <input type="text" class="form-control" id="parentName" name="parentName">
       </div>
   <div class="mb-3">
-    <label for="pCity" class="form-label">Parent City</label>
-    <input type="text" class="form-control" id="pCity" name="pCity">
+    <label for="parentCity" class="form-label">Parent City</label>
+    <input type="text" class="form-control" id="parentCity" name="parentCity">
   </div>
           <input type="hidden" name="actionType" value="Add">
    <button type="submit" class="btn btn-primary">Add</button>
