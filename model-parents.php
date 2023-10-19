7035e86fb5648516fd4131b486f90b9b5e6e0c70
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-18 20:52:43 -0500

    Update model-parents.php

--- model-parents.php
+++ model-parents.php
@@ -12,4 +12,17 @@ function selectParents() {
         throw $e;
     }
 }
+function insertParent($parentName, $parentCity) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `parents` (`parent_name`, `city_name`) VALUES (?,?);");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
