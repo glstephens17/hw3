fce2cad576507d059bd776687e361e9d93178ec1
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-18 20:58:50 -0500

    Update model-parents.php

--- model-parents.php
+++ model-parents.php
@@ -16,10 +16,10 @@ function insertParent($parentName, $parentCity) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("INSERT INTO `parents` (`parent_name`, `city_name`) VALUES (?,?);");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
+        $stmt->bind_param("ss", $parentName, $parentCity);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
