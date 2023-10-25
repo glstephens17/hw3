15cace77c10985997e55a62bfec4bbfc3001e66e
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-24 23:17:52 -0500

    Update view-parents.php

--- view-parents.php
+++ view-parents.php
@@ -37,7 +37,7 @@ include "view-parents-newform.php";
 <input type="hidden" name="pId" value="<?php echo $parent['parent_id'];?>"/>
 <input type="hidden" name="actionType" value="Delete">
 <button type="submit" class="btn btn-primary" onClick="return confirm('Are
-you sure ?')">
+you sure you want to Delete ?')">
 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
 fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5
@@ -47,7 +47,7 @@ fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1
 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5
 3h11V2h-11v1Z"/>
-</svg></button></form>
+</svg></form>
        </td>
    </tr>
  <?php  
