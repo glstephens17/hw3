5a37c4e16189b9000ddc3caf6c1e9683c7a85a8f
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 06:37:55 -0500

    Update view-parents-with-kids.php

--- view-parents-with-kids.php
+++ view-parents-with-kids.php
@@ -10,60 +10,43 @@
 </div>
 <div class="card-group">
   <?php
-  $cardsInRow = 0; // Initialize a counter for cards in the current row
-
   while ($parent = $parents->fetch_assoc()) {
-    if ($cardsInRow === 0) {
-      echo '<div class="row">'; // Start a new row
-    }
   ?>
-  <div class="col-md-3">
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title"><?php echo $parent['parent_name']; ?></h5>
-        <p class="card-text">
-          <ul class="list-group">
-            <?php
-            $kids = selectKidsByParent($parent['parent_id']);
-            while ($kid = $kids->fetch_assoc()) {
-            ?>
-            <li class="list-group-item">
-              <?php echo $kid['kid_name']; ?> -
-              <?php echo $kid['kid_birthyear']; ?> 
-              <div class="btn-group" role="group">
-                <?php include "view-parents-with-kids-editform.php"; ?>
-                <form method="post" action="">
-                  <input type="hidden" name="kid" value="<?php echo $kid['kid_id']; ?>">
-                  <input type="hidden" name="actionType" value="Delete">
-                  <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure you want to Delete?')">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                      <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                      <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                    </svg>
-                  </button>
-                </form>
-              </div>
-            </li>
-            <?php
-            }
-            ?>
-          </ul>
-        </p>
-        <p class="card-text"><small class="text-body-secondary">City: <?php echo $parent['city_name']; ?></small></p>
-      </div>
+  <div class="card">
+    <div class="card-body">
+      <h5 class="card-title"><?php echo $parent['parent_name']; ?></h5>
+      <p class="card-text">
+        <ul class="list-group">
+          <?php
+          $kids = selectKidsByParent($parent['parent_id']);
+          while ($kid = $kids->fetch_assoc()) {
+          ?>
+          <li class="list-group-item">
+            <?php echo $kid['kid_name']; ?> -
+            <?php echo $kid['kid_birthyear']; ?> 
+            <div class="btn-group" role="group">
+            <?php include "view-parents-with-kids-editform.php"; ?>
+            <form method="post" action="">
+              <input type="hidden" name="kid" value="<?php echo $kid['kid_id']; ?>">
+              <input type="hidden" name="actionType" value="Delete">
+              <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure you want to Delete?')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+                </svg>
+              </button>
+            </form>
+            </div>
+          </li>
+          <?php
+          }
+          ?>
+        </ul>
+      </p>
+      <p class="card-text"><small class="text-body-secondary">City: <?php echo $parent['city_name']; ?></small></p>
     </div>
   </div>
   <?php
-    $cardsInRow++; // Increment the card counter
-
-    if ($cardsInRow === 4) {
-      echo '</div>'; // End the current row after displaying 4 cards
-      $cardsInRow = 0; // Reset the card counter for the next row
-    }
-  }
-  
-  if ($cardsInRow !== 0) {
-    echo '</div>'; // Close the last row if it's not already closed
   }
   ?>
 </div>
