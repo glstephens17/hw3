22f52f003318483dd1725b548d7407c34575b46f
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 00:15:19 -0500

    Update view-parents-with-kids-newform.php

--- view-parents-with-kids-newform.php
+++ view-parents-with-kids-newform.php
@@ -1,44 +1,32 @@
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal"
-data-bs-target="#newKidsModal">
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-class="bi bi-plus-circle" viewBox="0 0 16 16">
-<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-<path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1
-0-1h3v-3A.5.5 0 0 1 8 4z"/>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#parentModal">
+<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
+  <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
 </svg>
 </button>
+
 <!-- Modal -->
-<div class="modal fade" id="newKidsModal" tabindex="-1"
-aria-labelledby="newKidsModalLabel" aria-hidden="true">
-<div class="modal-dialog">
-<div class="modal-content">
-<div class="modal-header">
-<h1 class="modal-title fs-5" id="newKidsModalLabel">New Kids</h1>
-<button type="button" class="btn-close" data-bs-dismiss="modal"
-aria-label="Close"></button>
-</div>
-<div class="modal-body">
-<form method="post" action="">
-<div class="mb-3">
-<label for="pId" class="form-label">Parent</label>
-<?php $parentList=selectParentsForInput();
-$selectedParent = 0;
-include "view-parent-input-list.php"; ?>
-</div>
-<div class="mb-3">
-<label for="kid_name" class="form-label">Kid Name</label>
-<input type="text" class="form-control" id="kid_name" name="kid_name">
-</div>
-<div class="mb-3">
-<label for="kid_birthyear" class="form-label">Birth Year</label>
-<input type="text" class="form-control" id="kid_birthyear"
-name="kid_birthyear">
-</div>
-<input type="hidden" name="actionType" value="Add">
-<button type="submit" class="btn btn-primary">Save</button>
+<div class="modal fade" id="parentModal" tabindex="-1" aria-labelledby="parentModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="parentModalLabel">New Parent</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="">
+  <div class="mb-3">
+    <label for="pName" class="form-label">Parent Name</label>
+    <input type="text" class="form-control" id="pName" name="pName">
+      </div>
+  <div class="mb-3">
+    <label for="pCity" class="form-label">Parent City</label>
+    <input type="text" class="form-control" id="pCity" name="pCity">
+  </div>
+          <input type="hidden" name="actionType" value="Add">
+   <button type="submit" class="btn btn-primary">Add</button>
 </form>
-</div>
-</div>
-</div>
+          </div>
+        </div>
+  </div>
 </div>
