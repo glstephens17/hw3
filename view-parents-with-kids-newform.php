6d3194fb732a11e8e57972ca85fe6d2944b22fab
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 00:18:29 -0500

    Update view-parents-with-kids-newform.php

--- view-parents-with-kids-newform.php
+++ view-parents-with-kids-newform.php
@@ -1,4 +1,4 @@
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#parentModal">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#kidModal">
 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
   <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
   <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
@@ -6,22 +6,26 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="parentModal" tabindex="-1" aria-labelledby="parentModalLabel" aria-hidden="true">
+<div class="modal fade" id="kidModal" tabindex="-1" aria-labelledby="kidModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="parentModalLabel">New Parent</h1>
+        <h1 class="modal-title fs-5" id="kidModalLabel">New Kid</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
   <div class="mb-3">
-    <label for="pName" class="form-label">Parent Name</label>
-    <input type="text" class="form-control" id="pName" name="pName">
+    <label for="kid_name" class="form-label">Kid Name</label>
+    <input type="text" class="form-control" id="kid_name" name="kid_name">
+      </div>
+<div class="mb-3">
+    <label for="kid_birthyear" class="form-label">Kid Birth Year</label>
+    <input type="text" class="form-control" id="kid_birthyear" name="kid_birthyear">
       </div>
   <div class="mb-3">
-    <label for="pCity" class="form-label">Parent City</label>
-    <input type="text" class="form-control" id="pCity" name="pCity">
+    <label for="pid" class="form-label">Parent Id</label>
+    <input type="text" class="form-control" id="pid" name="pid">
   </div>
           <input type="hidden" name="actionType" value="Add">
    <button type="submit" class="btn btn-primary">Add</button>
