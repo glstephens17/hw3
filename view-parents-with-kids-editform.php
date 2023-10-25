c73ef6d19ff9b5042322828a13998d613380e9b9
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 06:48:25 -0500

    Update view-parents-with-kids-editform.php

--- view-parents-with-kids-editform.php
+++ view-parents-with-kids-editform.php
@@ -1,31 +1,31 @@
 
- <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editParentModal<?php echo $parent['parent_id'];?>">
+ <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editParentModal<?php echo $kid['kid_id'];?>">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
           <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
         </svg>
          </button>
         <!-- Modal -->
-<div class="modal fade" id="editParentModal<?php echo $parent['parent_id'];?>" tabindex="-1" aria-labelledby="editParentModal<?php echo $parent['parent_id'];?>" aria-hidden="true">
+<div class="modal fade" id="editParentModal<?php echo $kid['kid_id'];?>" tabindex="-1" aria-labelledby="editParentModal<?php echo $kid['kid_id'];?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="editParentModalLabel<?php echo $parent['parent_id'];?>">Edit Parent</h1>
+        <h1 class="modal-title fs-5" id="editParentModalLabel<?php echo $kid['kid_id'];?>">Edit Kid</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <form method="post" action="">
   <div class="mb-3">
-    <label for="kid_name" class="form-label">Kid Name</label>
-    <input type="text" class="form-control" id="kid_name" name="kid_name">
+    <label for="kid_name<?php echo $kid['kid_id'];?>" class="form-label">Kid Name</label>
+    <input type="text" class="form-control" id="kid_name<?php echo $kid['kid_id'];?>" value="<?php echo $kid['kid_name'];?>" name="kid_name">
       </div>
 <div class="mb-3">
-    <label for="kid_birthyear" class="form-label">Kid Birth Year</label>
-    <input type="text" class="form-control" id="kid_birthyear" name="kid_birthyear">
+    <label for="kid_birthyear<?php echo $kid['kid_id'];?>" class="form-label">Kid Birth Year</label>
+    <input type="text" class="form-control" id="kid_birthyear<?php echo $kid['kid_id'];?>" value="<?php echo $kid['kid_birthyear'];?>" name="kid_birthyear">
       </div>
   <div class="mb-3">
-    <label for="pid" class="form-label">Parent Id</label>
-    <input type="text" class="form-control" id="pid" name="pid">
+    <label for="pid<?php echo $kid['kid_id'];?>" class="form-label">Parent Id</label>
+    <input type="text" class="form-control" id="pid<?php echo $kid['kid_id'];?>" value="<?php echo $kid['parent_id'];?>" name="pid">
   </div>
           <input type="hidden" name="actionType" value="Edit">
    <button type="submit" class="btn btn-primary">Add</button>
