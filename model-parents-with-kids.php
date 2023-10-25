7ccf632774aafb864d320f7abf780492f0c2f093
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 06:57:22 -0500

    Update model-parents-with-kids.php

--- model-parents-with-kids.php
+++ model-parents-with-kids.php
@@ -41,19 +41,21 @@ $conn->close();
 throw $e;
 }
 }
-function updateParentWithKids($kid_name, $kid_birthyear, $pid,$kid) {
+
+function updateParentWithKids($kid_name, $kid_birthyear, $pid, $kid) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("UPDATE `kids` set `$kid_name` = ?, `$kid_birthyear` = ?, `parent_id` = ? where kid_id=?");
-        $stmt->bind_param("ssii", $kid_name, $kid_birthyear, $pid,$kid);
+        $stmt = $conn->prepare("UPDATE `kids` SET kid_name = ?, kid_birthyear = ?, parent_id = ? WHERE kid_id = ?");
+        $stmt->bind_param("ssii", $kid_name, $kid_birthyear, $pid, $kid);
         $success = $stmt->execute();
-            $conn->close();
+        $conn->close();
         return $success;
     } catch (Exception $e) {
         $conn->close();
         throw $e;
     }
 }
+
 
 function insertParentWithKids( $kid_name, $kid_birthyear, $pid) {
 try {
