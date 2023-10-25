b062cdeb15ad72413eed5b1cb8bfef57d627bccc
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 07:05:59 -0500

    Update view-parent-input-list.php

--- view-parent-input-list.php
+++ view-parent-input-list.php
@@ -1,4 +1,4 @@
-<select class="form-select" id="pId" name="pId">
+<select class="form-select" id="pid" name="pid">
 <?php while ($parentItem = $parentList->fetch_assoc()) {
 $selText="";
 if($selectedParent==$parentItem['parent_id'])
@@ -6,7 +6,7 @@ if($selectedParent==$parentItem['parent_id'])
 $selText=" selected";
 }
 ?>
-<option value="<?php echo $parentItem['parent_id']; ?>"<?=$selText?>><?php echo
+<option value="<?php echo $kid['parent_id'];?>"<?=$selText?>><?php echo
 $parentItem['parent_name']; ?></option>
 <?php } ?>
 </select>
