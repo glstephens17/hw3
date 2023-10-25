a49fef406051c855ae91fa58985ab49836ae4eb6
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-10-25 00:10:15 -0500

    Update view-parents-with-kids.php

--- view-parents-with-kids.php
+++ view-parents-with-kids.php
@@ -1,12 +1,4 @@
-<div class="row">
-<div class="col">
 <h1>Parents with Kids</h1>
-</div>
-<div class="col-auto">
-<?php
-include "view-parents-with-kids-newform.php";
-?>
-</div>
   <div class="card-group">
 <?php
 while ($parent = $parents-> fetch_assoc())
