95171649a4d5d59455932edc443b2294a4f6213b
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-02 19:38:16 -0500

    Create view-pets.php

---
+++ view-pets.php
@@ -0,0 +1,35 @@
+<h1>Kids</h1>
+<?php
+include "view-pets-newform.php";
+?>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <tr>
+      <th>ID</th>
+      <th>Pet Name</th>
+      <th>Pet Breed</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+ <?php
+ while ($pet = $pets->fetch_assoc()) {
+ ?>
+   <tr>
+     <td><?php echo $pet['pet_id']; ?></td>
+     <td><?php echo $pet['pet_name']; ?></td>
+     <td><?php echo $pet['pet_breed']; ?></td>
+     <td>
+  <form method="delete" action="pets.php">
+    <input type="hidden" name="petID" value="<?php echo $pet['pet_id'];?>"/>
+    <button type="submit" class="btn btn-primary">Delete</button>
+  </form>
+     </td>     
+   </tr>
+ <?php  
+ }
+ ?>      
+    </tbody>
+  </table>
+</div>
