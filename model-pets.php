d9846740553672eb8564060fa92508d13a845134
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-02 19:33:43 -0500

    Update model-pets.php

--- model-pets.php
+++ model-pets.php
@@ -1,1 +1,54 @@
-
+<?php
+function selectPets() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT * FROM `pet`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function insertKid($kName, $kBirthYear, $pID) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `kids` (`kid_name`, `kid_birthyear`, `parent_id`) VALUES (?,?,?);");
+        $stmt->bind_param("ssi", $kName, $kBirthYear, $pID);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function updateParent($parentName, $parentCity, $pId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `parents` set `parent_name` = ?, `city_name` = ? WHERE `parent_id` = ?");
+        $stmt->bind_param("ssi", $parentName, $parentCity, $pId);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+function deleteParent($pId) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `parents` WHERE `parent_id` = ?");
+        $stmt->bind_param("i", $pId);
+        $success = $stmt->execute();
+            $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
