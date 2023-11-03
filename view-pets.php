bceae2c25709955060456b9659e01e0274b27717
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-02 19:52:23 -0500

    Update view-pets.php

--- view-pets.php
+++ view-pets.php
@@ -10,6 +10,8 @@ include "view-pets-newform.php";
       <th>Pet Name</th>
       <th>Pet Breed</th>
         <th></th>
+        <th></th>
+        <th></th>
       </tr>
     </thead>
     <tbody>
@@ -20,6 +22,23 @@ include "view-pets-newform.php";
      <td><?php echo $pet['pet_id']; ?></td>
      <td><?php echo $pet['pet_name']; ?></td>
      <td><?php echo $pet['pet_breed']; ?></td>
+     <td><a href="parents-pets.php?id=<?php echo $parent['parent_id']; ?>"</a><td>
+       <td>
+          <?php
+    include "view-pet-editform.php";
+    ?></td>
+   <td>
+  <form method="post" action="">
+    <input type="hidden" name="pId" value="<?php echo $pet['pet_id']; ?>"/>
+    <input type="hidden" name="actionType" value="Delete">
+    <button type="submit" class="btn btn-primary" onclick="return confirm('Are you sure you want to Delete?')">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+      </svg>
+    </button>
+  </form>
+</td>
      <td>
   <form method="delete" action="pets.php">
     <input type="hidden" name="petID" value="<?php echo $pet['pet_id'];?>"/>
