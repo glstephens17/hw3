1dd2b3742a2ca2fc6d362a413a9823b2b9f1861c
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-15 23:05:00 -0600

    Update pets.php

--- pets.php
+++ pets.php
@@ -1,6 +1,4 @@
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>  
-<canvas id="my-chart"></canvas>
-        
+
 <?php
 require_once("util-db.php");
 require_once("model-pets.php");
@@ -41,31 +39,3 @@ $pets = selectPets();
 include "view-pets.php";
 include "view-footer.php";
 ?>
-<script>  
-    var canvas = document.getElementById("my-chart");  
-  
-    new Chart(canvas, {  
-      type: 'bar',  
-      data: {  
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],  
-        datasets: [{  
-          label: 'Sales',  
-          data: [12, 19, 3, 5, 2, 3, 8],  
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',  
-          borderColor: 'rgba(54, 162, 235, 1)',  
-          borderWidth: 1  
-        }]  
-      },  
-      options: {  
-        responsive: true,  
-        scales: {  
-          yAxes: [{  
-            ticks: {  
-              beginAtZero: true  
-            }  
-          }]  
-        }  
-      }   
-  });  
-</script>  
-
