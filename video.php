6a6671ba5d282139f4f85d8a3c27bb952a6a08a2
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-16 00:09:33 -0600

    Update video.php

--- video.php
+++ video.php
@@ -1,12 +1,14 @@
-<head>  
-  <link rel="stylesheet" href="https://cdn.plyr.io/3.6.4/plyr.css">  
-  <script src="https://cdn.plyr.io/3.6.4/plyr.js"></script>  
-</head>  
+<link rel="stylesheet" href="https://cdn.plyr.io/3.6.4/plyr.css">  
+  <script src="https://cdn.plyr.io/3.6.4/plyr.js"></script>   
+<?php
+
+$pageTitle = "3D Animation";
+include "view-header.php";
+include "view-footer.php";
+?>
 <video id="myVideo" controls>  
   <source src="video.mp4" type="video/mp4">  
 </video>  
-<body>  
   <script>  
     const player = new Plyr('#myVideo');  
-  </script>  
-</body>
+  </script> 
