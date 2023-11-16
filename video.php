3f7899613c1d9882dbda51eca422eeac940b1264
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-16 00:08:24 -0600

    Create video.php

---
+++ video.php
@@ -0,0 +1,12 @@
+<head>  
+  <link rel="stylesheet" href="https://cdn.plyr.io/3.6.4/plyr.css">  
+  <script src="https://cdn.plyr.io/3.6.4/plyr.js"></script>  
+</head>  
+<video id="myVideo" controls>  
+  <source src="video.mp4" type="video/mp4">  
+</video>  
+<body>  
+  <script>  
+    const player = new Plyr('#myVideo');  
+  </script>  
+</body>
