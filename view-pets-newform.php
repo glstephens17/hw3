69e06fd6cc9373a9257127659eb5434adc3e7a20
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-15 22:41:44 -0600

    Update view-pets-newform.php

--- view-pets-newform.php
+++ view-pets-newform.php
@@ -1,5 +1,5 @@
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>  
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#petModal">
+<button id="submit-button" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#petModal">
 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-person-add" viewBox="0 0 16 16">
   <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7Zm.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0Zm-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0ZM8 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4Z"/>
   <path d="M8.256 14a4.474 4.474 0 0 1-.229-1.004H3c.001-.246.154-.986.832-1.664C4.484 10.68 5.711 10 8 10c.26 0 .507.009.74.025.226-.341.496-.65.804-.918C9.077 9.038 8.564 9 8 9c-5 0-6 3-6 4s1 1 1 1h5.256Z"/>
@@ -25,12 +25,12 @@
     <input type="text" class="form-control" id="pBreed" name="pBreed">
   </div>
     <input type="hidden" name="actionType" value="Add">
-   <button id="submit-button" type="submit" class="btn btn-primary">Add</button>
+   <button type="submit" class="btn btn-primary">Add</button>
 </form>
         <script>  
   $(document).ready(function() {  
     $('#submit-button').click(function() {  
-      Swal.fire('Your Pet has been added!');  
+      Swal.fire('You are about to add a new Pet!');  
     });  
   });  
 </script>
