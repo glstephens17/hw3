f75093ef3246bbaff4c3c6334bb87f55622a77d7
glstephens17 <123123729+glstephens17@users.noreply.github.com>
2023-11-15 22:28:18 -0600

    Update view-pet-editform.php

--- view-pet-editform.php
+++ view-pet-editform.php
@@ -1,4 +1,4 @@
- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>  
+
 <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editPetModal<?php echo $pet['pet_id'];?>">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
           <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
@@ -7,7 +7,8 @@
          </button>
         <!-- Modal -->
 <div class="modal fade" id="editPetModal<?php echo $pet['pet_id'];?>" tabindex="-1" aria-labelledby="editPetModal<?php echo $pet['pet_id'];?>" aria-hidden="true">
-  <div class="modal-dialog">
+ <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>  
+ <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
         <h1 class="modal-title fs-5" id="editPetModalLabel<?php echo $pet['pet_id'];?>">Edit Pet</h1>
@@ -26,16 +27,14 @@
           <input type="hidden" name="pId" value="<?php echo $pet['pet_id'];?>">
           <input type="hidden" name="actionType" value="Edit">
    <button id="submit-button" type="submit" class="btn btn-primary">Add</button>
-</form>
-      
-  
-<script>  
+         <script>  
   $(document).ready(function() {  
     $('#submit-button').click(function() {  
       Swal.fire('Your Pet has been edited!');  
     });  
   });  
 </script>
+</form>
           </div>
         </div>
   </div>
